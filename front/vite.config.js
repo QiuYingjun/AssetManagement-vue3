694d645dab536beb1628fbbdc1fc5c1b60b2cc27
qiuyj@sohobb.jp <qiuyj@sohobb.jp>
2021-09-15 11:03:30 +0900

    改服务器ip

--- front/vite.config.js
+++ front/vite.config.js
@@ -4,10 +4,11 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: "/AssetManagement-vue3/",
   server: {
     proxy: {
       "^/rest.*": {
-        target: "http://localhost:5000",
+        target: "http://127.0.0.1:5000",
         changeOrigin: true,
       },
       // "/rest/*": {
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 # AssetManagement-vue3
 
 资产管理vue3版，前后端分离。
+
+[预览](https://qiuyingjun.github.io/AssetManagement-vue3/)
